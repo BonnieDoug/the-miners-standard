c3ea69c7e0b940f38cfd31ca7da28b3f55a2635c
Doug <doug@bonniechef.com>
2018-07-02 23:06:45 +0100

    initial commit

---
+++ layouts/default.vue
@@ -0,0 +1,138 @@
+<template>
+        <v-app light>
+            <v-navigation-drawer
+                    class="hidden-md-and-up"
+                    v-model="menu"
+                    fixed
+                    app
+                    right
+                    clipped
+            >
+                <v-list dense>
+                    <template v-for="item in menuItems">
+                        <v-list-tile :key="item.name" @click="">
+                            <v-list-tile-action>
+                            <v-icon>{{ item.icon }}</v-icon>
+                            </v-list-tile-action>
+                            <v-list-tile-content>
+                                <v-list-tile-title>
+                                    {{ item.name }}
+                                </v-list-tile-title>
+                            </v-list-tile-content>
+                        </v-list-tile>
+                    </template>
+                </v-list>
+            </v-navigation-drawer>
+            <v-toolbar class="black" dark fixed clipped-right>
+                <v-toolbar-title v-text="title">
+                </v-toolbar-title>
+                <v-spacer></v-spacer>
+                <span class="hidden-sm-and-down">
+              <v-btn v-for="item, key in menuItems" flat :key="`menu-item-${key}`">{{ item.name }}</v-btn>
+            </span>
+                <span class="hidden-md-and-up">
+                <v-btn dark icon @click.stop="menu = !menu"><v-icon>menu</v-icon></v-btn>
+            </span>
+            </v-toolbar>
+            <v-content>
+                <nuxt/>
+                <section>
+                    <v-container grid-list-xl>
+                        <v-layout row wrap justify-center class="my-5">
+                            <v-flex xs12 sm4>
+                                <v-card class="elevation-0 transparent">
+                                    <v-card-title primary-title class="layout justify-center">
+                                        <div class="headline">Company info</div>
+                                    </v-card-title>
+                                    <v-card-text>
+                                        Cras facilisis mi vitae nunc lobortis pharetra. Nulla volutpat tincidunt ornare.
+                                        Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac
+                                        turpis
+                                        egestas.
+                                        Nullam in aliquet odio. Aliquam eu est vitae tellus bibendum tincidunt.
+                                        Suspendisse
+                                        potenti.
+                                    </v-card-text>
+                                </v-card>
+                            </v-flex>
+                            <v-flex xs12 sm4 offset-sm1>
+                                <v-card class="elevation-0 transparent">
+                                    <v-card-title primary-title class="layout justify-center">
+                                        <div class="headline">Contact us</div>
+                                    </v-card-title>
+                                    <v-card-text>
+                                        Cras facilisis mi vitae nunc lobortis pharetra. Nulla volutpat tincidunt ornare.
+                                    </v-card-text>
+                                    <v-list class="transparent">
+                                        <v-list-tile>
+                                            <v-list-tile-action>
+                                                <v-icon class="blue--text text--lighten-2">phone</v-icon>
+                                            </v-list-tile-action>
+                                            <v-list-tile-content>
+                                                <v-list-tile-title>777-867-5309</v-list-tile-title>
+                                            </v-list-tile-content>
+                                        </v-list-tile>
+                                        <v-list-tile>
+                                            <v-list-tile-action>
+                                                <v-icon class="blue--text text--lighten-2">place</v-icon>
+                                            </v-list-tile-action>
+                                            <v-list-tile-content>
+                                                <v-list-tile-title>Chicago, US</v-list-tile-title>
+                                            </v-list-tile-content>
+                                        </v-list-tile>
+                                        <v-list-tile>
+                                            <v-list-tile-action>
+                                                <v-icon class="blue--text text--lighten-2">email</v-icon>
+                                            </v-list-tile-action>
+                                            <v-list-tile-content>
+                                                <v-list-tile-title>john@vuetifyjs.com</v-list-tile-title>
+                                            </v-list-tile-content>
+                                        </v-list-tile>
+                                    </v-list>
+                                </v-card>
+                            </v-flex>
+                        </v-layout>
+                    </v-container>
+                </section>
+
+                <v-footer class="blue darken-2">
+                    <v-layout row wrap align-center>
+                        <v-flex xs12>
+                            <div class="white--text ml-3">
+                                Made with
+                                <v-icon class="red--text">favorite</v-icon>
+                                by <a class="white--text" href="https://vuetifyjs.com" target="_blank">Vuetify</a>
+                                and <a class="white--text" href="https://github.com/vwxyzjn">Costa Huang</a>
+                            </div>
+                        </v-flex>
+                    </v-layout>
+                </v-footer>
+            </v-content>
+        </v-app>
+</template>
+<script>
+  export default {
+    data () {
+      return {
+        clipped: false,
+        menu: false,
+        fixed: false,
+        items: [
+          {icon: 'apps', title: 'Welcome', to: '/'},
+          {icon: 'bubble_chart', title: 'Inspire', to: '/inspire'}
+        ],
+        miniVariant: false,
+        right: true,
+        rightDrawer: false,
+        title: 'The Miners Standard',
+        menuItems: [
+          {name: 'Home', to: '', icon: 'home'},
+          {name: 'News & Events', to: '', icon: 'event'},
+          {name: 'About', to: '', icon: 'help'},
+          {name: 'Find Us', to: '', icon: 'place'},
+          {name: 'Contact Us', to: '', icon: 'phone'}
+        ]
+      }
+    }
+  }
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,99 @@
+<style>
+    @media (max-width: 510px) {
+        .hero-1 img {
+            left: 118%;
+        }
+    }
+</style>
+<template>
+    <div>
+        <section>
+            <v-parallax src="/images/hero1.jpg" height="650" class="hero-1">
+                <v-layout
+                        column
+                        align-center
+                        justify-center
+                        class="white--text"
+                >
+                    <h1 class="white--text mb-2 display-1 text-xs-center" style="line-height: 3.5rem !important; max-width: 400px; font-size: 400% !important; text-shadow: 2px 2px #111;">The Miners Standard</h1>
+                    <div class="subheading mb-3 text-xs-center" style="text-shadow: 2px 2px #111; font-size: 140% !important;">A good old fashioned British Pub</div>
+                </v-layout>
+            </v-parallax>
+        </section>
+
+        <section>
+            <v-layout
+                    column
+                    wrap
+                    class="my-5"
+                    align-center
+            >
+                <v-flex xs12 sm4 class="my-3">
+                    <div class="text-xs-center">
+                        <h2 class="headline">The best way to start developing</h2>
+                        <span class="subheading">
+                Cras facilisis mi vitae nunc
+              </span>
+                    </div>
+                </v-flex>
+                <v-flex xs12>
+                    <v-container grid-list-xl>
+                        <v-layout row wrap align-center>
+                            <v-flex xs12 md4>
+                                <v-card class="elevation-0 transparent">
+                                    <v-card-text class="text-xs-center">
+                                        <v-icon x-large class="blue--text text--lighten-2">color_lens</v-icon>
+                                    </v-card-text>
+                                    <v-card-title primary-title class="layout justify-center">
+                                        <div class="headline text-xs-center">Material Design</div>
+                                    </v-card-title>
+                                    <v-card-text>
+                                        Cras facilisis mi vitae nunc lobortis pharetra. Nulla volutpat tincidunt ornare.
+                                        Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac
+                                        turpis egestas.
+                                        Nullam in aliquet odio. Aliquam eu est vitae tellus bibendum tincidunt.
+                                        Suspendisse potenti.
+                                    </v-card-text>
+                                </v-card>
+                            </v-flex>
+                            <v-flex xs12 md4>
+                                <v-card class="elevation-0 transparent">
+                                    <v-card-text class="text-xs-center">
+                                        <v-icon x-large class="blue--text text--lighten-2">flash_on</v-icon>
+                                    </v-card-text>
+                                    <v-card-title primary-title class="layout justify-center">
+                                        <div class="headline">Fast development</div>
+                                    </v-card-title>
+                                    <v-card-text>
+                                        Cras facilisis mi vitae nunc lobortis pharetra. Nulla volutpat tincidunt ornare.
+                                        Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac
+                                        turpis egestas.
+                                        Nullam in aliquet odio. Aliquam eu est vitae tellus bibendum tincidunt.
+                                        Suspendisse potenti.
+                                    </v-card-text>
+                                </v-card>
+                            </v-flex>
+                            <v-flex xs12 md4>
+                                <v-card class="elevation-0 transparent">
+                                    <v-card-text class="text-xs-center">
+                                        <v-icon x-large class="blue--text text--lighten-2">build</v-icon>
+                                    </v-card-text>
+                                    <v-card-title primary-title class="layout justify-center">
+                                        <div class="headline text-xs-center">Completely Open Sourced</div>
+                                    </v-card-title>
+                                    <v-card-text>
+                                        Cras facilisis mi vitae nunc lobortis pharetra. Nulla volutpat tincidunt ornare.
+                                        Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac
+                                        turpis egestas.
+                                        Nullam in aliquet odio. Aliquam eu est vitae tellus bibendum tincidunt.
+                                        Suspendisse potenti.
+                                    </v-card-text>
+                                </v-card>
+                            </v-flex>
+                        </v-layout>
+                    </v-container>
+                </v-flex>
+            </v-layout>
+        </section>
+    </div>
+</template>
