75fdc9a92eb98a4eba744f078ced482506204ac4
Doug <doug@bonniechef.com>
2018-11-28 14:11:00 +0000

    Event images

--- pages/news-and-events.vue
+++ pages/news-and-events.vue
@@ -32,7 +32,7 @@
                             >
                                 <v-layout row wrap>
                                     <v-flex xs12 md6 v-for="event, key in events" :key="key">
-                                        <v-card class="elevation-6">
+                                        <v-card class="elevation-6" small>
                                             <v-card-media
                                                     class="white--text"
                                                     height="300px"
@@ -40,13 +40,13 @@
                                             >
                                                 <v-container fill-height fluid>
                                                     <v-layout fill-height>
-                                                        <v-flex xs12 align-end flexbox>
-                                                            <span class="headline">{{ event.name }}</span>
+                                                        <v-flex xs12 align-end flexbox class="ma-2 pa-2">
+                                                            <span class="headline pa-3" :style="'background-color:' + event.colour">{{ event.name }}</span>
                                                         </v-flex>
                                                     </v-layout>
                                                 </v-container>
                                             </v-card-media>
-                                            <v-card-text>
+                                            <v-card-text style="min-height:150px" class="pb-0 mb-0">
                                                 <h3 class="pt-2 pb-0 mb-0"><strong>{{ event.date }}</strong></h3>
                                                 <v-divider></v-divider>
                                                 <p class="subheading py-2">{{ event.description }}</p>
@@ -74,27 +74,31 @@
         events: [
           {
             name: 'Visit from the Guisers',
-            description: 'Bacon ipsum dolor amet meatball buffalo pork loin meatloaf, t-bone ribeye ham sausage salami pastrami kevin venison short loin spare ribs. Pork chop bresaola pork belly short ribs. Drumstick turducken beef, ball tip filet mignon pork belly shankle bacon.',
+            description: 'The Winster Guisers are in town!!  Join us at The Miners Standard to witness this entertaining and amusing performance.',
             date: 'Saturday 22nd December 2018',
-            image: 'https://picsum.photos/800/300/?random'
+            image: '/images/events/guisers-1.jpg',
+            colour: '#000'
           },
           {
             name: 'Children\'s Christmas Party',
-            description: 'Running from 4pm till 6pm. Party food and entertainment will be provided',
+            description: 'The Miners Standard are hosting a Christmas party for kids of all ages!!  There will be music and dancing along with party games.  Party food will be provided as well as a selection of goodies to get you in the festive spirit.',
             date: 'Sunday 23rd December 2018',
-            image: 'https://picsum.photos/800/300/?random'
+            image: '/images/events/christmas-2.jpg',
+            colour: '#a32c17'
           },
           {
             name: 'Carol Singing',
-            description: 'Bacon ipsum dolor amet meatball buffalo pork loin meatloaf, t-bone ribeye ham sausage salami pastrami kevin venison short loin spare ribs. Pork chop bresaola pork belly short ribs. Drumstick turducken beef, ball tip filet mignon pork belly shankle bacon.',
+            description: 'The Miners Standard are continuing the tradition of the annual Carol Singing Service.  Bring your voices and join us for a mince pie at this festive event.',
             date: 'Sunday 30th December 2018',
-            image: 'https://picsum.photos/800/300/?random'
+            image: '/images/events/christmas-1.jpg',
+            colour: '#dd1530'
           },
           {
             name: 'New Years Eve Party',
-            description: 'Bacon ipsum dolor amet meatball buffalo pork loin meatloaf, t-bone ribeye ham sausage salami pastrami kevin venison short loin spare ribs. Pork chop bresaola pork belly short ribs. Drumstick turducken beef, ball tip filet mignon pork belly shankle bacon.',
+            description: 'New Years Eve Party - Wez and Lindsay invite you to The Miners Standard to celebrate the end of 2018 and the beginning of 2019.  Join us to let in the New Year with family and friends whilst enjoying music, party food and a wide selection of drinks.',
             date: 'Monday 31st December 2018',
-            image: 'https://picsum.photos/800/300/?random'
+            image: '/images/events/christmas-3.jpg',
+            colour: '#cb9a6c'
           }
         ]
       }
